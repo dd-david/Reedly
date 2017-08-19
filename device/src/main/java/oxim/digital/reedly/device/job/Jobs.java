fd7834bd0df6dcc20d94ac887e413c03f1c2c634
dd-david <dev.dream.developer@gmail.com>
2017-08-19 13:58:37 +0900

    comment: device(ex, jobscheduler, notificationmanager) wrapper manager

--- device/src/main/java/oxim/digital/reedly/device/job/Jobs.java
+++ device/src/main/java/oxim/digital/reedly/device/job/Jobs.java
@@ -2,6 +2,9 @@ package oxim.digital.reedly.device.job;
 
 import android.app.job.JobInfo;
 
+// 앞선 설명에서 Device module 의 interface 도 --> domain module 에 넣으라고 했는데
+// Job이나 Notifiacton 같은 interface 는 이렇게 내부에 두나?
+// 그럼 어떻게, 누가 활용하나? app module 에서 활용하는 것도 dependency injection 을 활용?
 public interface Jobs {
 
     int schedule(JobInfo jobInfo);
